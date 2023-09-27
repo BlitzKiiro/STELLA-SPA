1efa7cd4361af2db702f2a026eff5b7fcc0ea159
Ahmed Azmy <ahmedazmycs@protonmail.com>
2023-09-27 17:33:15 +0300

    Meta data update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,27 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
+import "aos/dist/aos.css";
+import { Noto_Kufi_Arabic } from "next/font/google";
+import AOS from "@/components/aos";
 export const metadata = {
-  title: "Stella SPA خدمات مساج متكاملة",
+  title: "Stella SPA - خدمات مساج متكاملة",
   description:
     "Stella SPA massage & care center, Saudi Arabia, ستيلا سبا لخدمات المساج في المملكة العربية السعودية",
+  other: {
+    "theme-color": "#958977",
+  },
 };
+
+const Kufi = Noto_Kufi_Arabic({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  display: "swap",
+  subsets: ["arabic"],
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={Kufi.className}>{children}</body>
+      <AOS />
     </html>
   );
 }
