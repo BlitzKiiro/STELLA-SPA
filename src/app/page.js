83747b4fb2b7d85c5cdaf42afefa86845a7e0094
Ahmed Azmy <ahmedazmycs@protonmail.com>
2023-09-27 21:59:12 +0300

    about section

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import About from "@/components/home/about/about";
 import HomeHeader from "@/components/home/header";
 import Navbar from "@/components/navbar";
 
@@ -7,6 +8,7 @@ export default function Home() {
       <HomeHeader>
         <Navbar />
       </HomeHeader>
+      <About />
     </main>
   );
 }
---
+++ src/components/home/about/about.jsx
@@ -0,0 +1,31 @@
+import { BranchIcon } from "@/components/custom/icons";
+
+const About = () => {
+  return (
+    <div
+      dir='rtl'
+      className='relative w-[90%] mx-auto my-32 flex flex-col md:flex-row items-center'
+      data-aos='fade-up'
+      data-aos-duration='1200'
+    >
+      <p className='md:rotate-90 text-center mb-[30px]'>من نحن</p>
+      <div className='mx-auto w-[70%] text-center '>
+        <div className='w-full flex justify-center mb-10'>
+          <BranchIcon />
+        </div>
+        <div>
+          <span className='text-white text-[28px] md:text-[38px] font-medium  leading-[48px] '>
+            في مركز ستيلا سبا{" "}
+          </span>
+          <span className='text-white text-center text-[28px] md:text-[38px] font-light  leading-[48px]'>
+            نقوم بتقديم أفضل خدمات التدليك والمساج بأيدي أمهر المختصين الذين
+            يعملون بحرفية تلبي كافة احتياجاتك للوصول للراحة المنشودة بأفضل
+            التقنيات والطرق الحديثة والمبتكرة والعلاجية المستخدمة
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
