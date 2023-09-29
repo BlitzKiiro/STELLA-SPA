1804af2d3cede11e6d21954a0d6f34e2c9ec9cdf
Ahmed Azmy <ahmedazmycs@protonmail.com>
2023-09-29 20:26:50 +0300

    Social Icons Linked

--- src/components/navbar/Social.jsx
+++ src/components/navbar/Social.jsx
@@ -1,18 +1,20 @@
 import Link from "next/link";
-import { FacebookIcon, InstagramIcon, XIcon } from "../custom/icons";
+import { TiktokIcon, SnapChatIcon } from "../custom/icons";
 
 const Social = () => {
   return (
     <div className='flex gap-5 items-center'>
-      <Link target='_blank_instagram' href={"https://www.instagram.com/"}>
-        <InstagramIcon />
+      <Link
+        target='_blank_instagram'
+        href={"https://www.tiktok.com/@stellaspa6"}
+      >
+        <TiktokIcon />
       </Link>
-      <Link target='_blank_fb' href={"https://www.facebook.com"}>
-        {" "}
-        <XIcon />
-      </Link>
-      <Link target='_blank_x' href={"https://www.x.com"}>
-        <FacebookIcon />
+      <Link
+        target='_blank_fb'
+        href={"https://www.snapchat.com/add/stellaspa23"}
+      >
+        <SnapChatIcon />
       </Link>
     </div>
   );
