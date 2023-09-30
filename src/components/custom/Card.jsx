93b75bb3b7ef242584bbc41ce695c32d02cbe232
Ahmed Azmy <ahmedazmycs@protonmail.com>
2023-09-30 13:18:56 +0300

    New Services Component

--- src/components/custom/Card.jsx
+++ src/components/custom/Card.jsx
@@ -14,32 +14,30 @@ const Card = ({ img, title, description }) => {
       data-aos='fade-left'
       data-aos-duration='1200'
       ref={hoverRef}
-      className='drop-shadow w-[350px] h-[300px] overflow-hidden relative mx-auto '
+      className='w-[95%] md:w-[370px] h-[570px] overflow-hidden relative mx-auto bg-white '
     >
-      <Image
-        src={img}
-        alt={title}
-        fill
-        className={cn("transition-all duration-500 scale-[1]", {
-          "!scale-[1.1]": isHover,
-        })}
-      />
-      <span
-        className={cn(
-          "transition-all duration-500 ",
-          "absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center bg-[rgba(0,0,0,0.5)]  opacity-0 ",
-          { " opacity-100 ": isHover }
-        )}
-      >
-        <p className='text-xl cursor-default'>{title}</p>
+      <div className='relative w-full h-[230px] overflow-hidden'>
+        <Image
+          src={img}
+          alt={title}
+          className={cn("transition-all duration-500 scale-[1] w-full", {
+            "!scale-[1.1]": isHover,
+          })}
+        />
+      </div>
+      <div className='w-full flex flex-col items-center justify-center  px-4 py-12 gap-6  '>
+        <h2 className='text-2xl cursor-default font-bold text-zinc-950'>
+          {title}
+        </h2>
+        <p className='text-gray-800 leading-8'> {description}</p>
         <Link
           href={`https://api.whatsapp.com/send/?phone=9660564527479&text&type=phone_number&app_absent=0&text=السلام عليكم، أرغب في حجز ${title}`}
           target='_blank_res'
-          className='absolute bottom-20 border border-white rounded-md py-1 px-6'
+          className='absolute bottom-10 border bg-[#6C6456] rounded-md py-1 px-6'
         >
           احجز الأن
         </Link>
-      </span>
+      </div>
     </div>
   );
 };
